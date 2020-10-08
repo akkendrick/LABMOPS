be0be86a00cd35f02622566afecdcbb713f14fd1
akkendrick <alexkend@stanford.edu>
2020-10-07 20:57:32 -0700

    Trying to read raw image files

--- Beadpack_sim.py
+++ Beadpack_sim.py
@@ -7,13 +7,15 @@ import openpnm as op
 import imageio
 import numpy as np
 import rawpy
-import imageio
-from PIL import Image
+import skimage.io
+from PIL import Image, ImageEnhance
 from porespy.filters import find_peaks, trim_saddle_points, trim_nearby_peaks
 from porespy.tools import randomize_colors
 from skimage.morphology import watershed
+from skimage import exposure
 from matplotlib import cm
-
+from scipy import misc 
+from skimage import data, io, filters
 ps.visualization.set_mpl_style()
 
 # data = np.fromfile("AW_DryScan_segm_650x650x294_8bitunsigned.raw")
@@ -21,7 +23,7 @@ ps.visualization.set_mpl_style()
 
 # #beadPack = np.ndarray.reshape(data,(650,650,254)) 
 # plt.imshow(beadPack[:,:,80])
-path = 'AW_DryScan_segm_650x650x294_8bitunsigned.raw'
+image = 'AW_DryScan_segm_650x650x294_8bitunsigned.tif'
 
 
 # raw = rawpy.imread(path)
@@ -32,14 +34,59 @@ path = 'AW_DryScan_segm_650x650x294_8bitunsigned.raw'
 
 # imageio.imsave('default.tiff', rgb)
 
-scene_infile = open(path,'rb')
-scene_image_array = np.fromfile(scene_infile,dtype=np.uint8,count=-1)
+# scene_infile = open(path,'rb')
+# scene_image_array = np.fromfile(scene_infile)
 
-beadPack = np.reshape(scene_image_array,(650,650,294)) 
+# beadPack = np.reshape(scene_image_array,(650,650,294)) 
 
-plt.imshow(beadPack[:,:,80],cmap=plt.cm.gray)
-test = (beadPack[:,:,80])
+# plt.imshow(beadPack[:,:,80],cmap=plt.cm.gray)
+# plt.show()
+
+# test = (beadPack[:,:,80])
+# img = Image.fromarray(beadPack[:,:,80])
+# img.save('testout.tiff')
+
+# This only does a 2D slice, no good
+# im = Image.open(image) 
+# factor = 100000
+
+# enhancer = ImageEnhance.Contrast(im)
+# im_output = enhancer.enhance(factor)
+
+# imarray = np.array(im_output) 
+# imarray.shape 
+
+# im_output.show()
+
+# im = np.memmap('AW_DryScan_segm_650x650x294_8bitunsigned.raw', dtype=np.ubyte, shape=(650,650,294))
+
+# test = im[:,:,30]
+# plt.imshow(test, cmap = plt.cm.gray)
+# plt.show()
+
+imname = 'RandomSpherePackingMicrostructure.raw'
+
+# im = skimage.io.imread(imname).astype(np.ubyte)
+
+data = np.memmap(imname, dtype=np=uint8)
+data.shape
+
+dataShape = data.reshape((565,525,1071))
+dataslice = dataShape[:,:,300]
+
+edges = filters.sobel(dataslice)
+
+data_eq = exposure.equalize_hist(dataslice)
+
+image = dataslice;
+plt.imshow(image,cmap='gray')
+plt.show()
 
 
-img = Image.fromarray(beadPack[:,:,80])
-img.save('my.tiff')
+
+
+ps.io.to_vtk(data,path='out.vtk',divide=False,downsample=False, voxel_size=1, vox=False)
+
+# from mayavi import mlab
+# mlab.contour3d(dataShape)
+# mlab.outline()
